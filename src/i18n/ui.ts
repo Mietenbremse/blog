0e924a7fe563306b22a0210c1c5d0d803e69132d
pmoellerfriedrich <pmoellerfriedrich@eos-uptrade.de>
2024-05-24 10:40:26 +0200

    add localization

---
+++ src/i18n/ui.ts
@@ -0,0 +1,18 @@
+export const languages = {
+    de: 'Deutsch',
+    en: 'English',
+  };
+  
+  export const defaultLang = 'de';
+  
+  export const ui = {
+    en: {
+      'nav.home': 'Home',
+      'nav.about': 'About',
+      'nav.twitter': 'Twitter',
+    },
+    de: {
+      'nav.home': 'Home',
+      'nav.about': 'Über uns',
+    },
+  } as const;
