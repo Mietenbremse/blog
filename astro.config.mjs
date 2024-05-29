a2258d17eecd2428b70aa5e5709daa5f875bfb79
Gregor Weber <git@dflate.io>
2024-05-29 16:49:36 +0200

    proper redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,27 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blog.mietencheck.de',
-  integrations: [mdx(), sitemap(), tailwind({
-    // Beispiel: Erlaubt das Verwenden verschachtelter CSS Deklarationen
-    // neben dem Tailwind Syntax
-    nesting: true,
-  }),],
+  site: "https://blog.mietencheck.de",
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind({
+      // Beispiel: Erlaubt das Verwenden verschachtelter CSS Deklarationen
+      // neben dem Tailwind Syntax
+      nesting: true,
+    }),
+  ],
   i18n: {
     defaultLocale: "de",
-    locales: ["de", "en", ],
+    locales: ["de", "en"],
     routing: {
-      prefixDefaultLocale: true
-  }
-  }
+      prefixDefaultLocale: true,
+    },
+  },
+  redirects: { "/": "/de" },
 });
