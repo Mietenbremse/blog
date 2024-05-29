4f117b7efe5e0dcde5a9fba089e61a34368c5643
Jonas Brinkhoff <jonasbrinkhoff@gmail.com>
2024-05-29 23:24:03 +0200

    update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Mietencheck.de';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'mietencheck.de';
+export const SITE_DESCRIPTION = 'Ganz Berlin zahlt zu viel Miete! Finde heraus, ob auch du betroffen bist und senke deine Mietkosten. Prüfe jetzt kostenlos und sicher deine Miete auf Mietencheck.de.';
